74b6cfacc75fb57a1b535731a1185496dea9e294
Jack Rimmer <jackrimmer2996@hotmail.co.uk>
2021-07-05 16:57:43 +0100

    Finished logic for wordifying clock times

--- challenge-6-clock-wise/src/index.js
+++ challenge-6-clock-wise/src/index.js
@@ -1,16 +1,20 @@
-const ONES = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'seven', 'eight', 'nine'];
+const ONES = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 const TEENS = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',' seventeen', 'eighteen', 'nineteen'];
 const TENS = ['ten', 'twenty', 'thirty', 'forty', 'fifty'];
 
+const submitTime = document.querySelector('#submit-time');
+const anglicisedTime = document.querySelector('#anglicised-time');
+const playAudio = document.querySelector('#play-audio');
+
 const angliciseTime = (time) => {
-    const hour = time.substring(0, 1);
-    const minute = time.substring(3, 4);
-    const meridiem = parseInt(hour) <= 12 ? 'am' : 'pm';
+    const hour = time.substring(0, 2);
+    const minute = time.substring(3, 5);
+    const meridiem = parseInt(hour) < 12 || parseInt(hour) === 24 ? 'am' : 'pm';
 
     const anglicisedHour = angliciseHour(hour);
     const anglicisedMinute = angliciseMinute(minute);
 
-    return `It is ${anglicisedHour} ${anglicisedMinute} ${meridiem}`;
+    return `It is ${anglicisedHour} ${anglicisedMinute} ${meridiem}.`;
 }
 
 const angliciseHour = (hour) => {
@@ -18,29 +22,41 @@ const angliciseHour = (hour) => {
 
     if (hour[0] === '0')
         return ONES[parseInt(hour[1]) - 1];
-    else if (hour[0] === 1 && hour[1] !== '0')
+    else if (hour[0] === '1' && hour[1] !== '0')
         return TEENS[parseInt(hour[1]) - 1];
     else
         return TENS[0];
 }
 
-const twelvifyHour = (twelvifiedHour) => {
-    twelvifiedHour = parseInt(twelvifiedHour) % 12;
+const twelvifyHour = (hour) => {
+    twelvifiedHour = parseInt(hour) % 12;
     if (twelvifiedHour === 0)
         twelvifiedHour = 12;
+
+    if (twelvifiedHour < 10)
+        twelvifiedHour = `0${twelvifiedHour}`;
 
     return twelvifiedHour.toString();
 }
 
 const angliciseMinute = (minute) => {
     if (minute[0] === '0')
-        return minute[1] === 0 ? `o'clock` : `o'${ONES[parseInt(minute[1]) - 1]}`;
-    else if (minute[0] === 1 && minute[1] !== '0')
+        return minute[1] === '0' ? `` : `o'${ONES[parseInt(minute[1]) - 1]}`;
+    else if (minute[0] === '1' && minute[1] !== '0')
         return TEENS[parseInt(minute[1]) - 1];
     else {
         if (minute[1] === '0')
-            return TENS[minute[0] - 1];
+            return TENS[parseInt(minute[0] - 1)];
         else
-            return `${TENS[minute[0] - 1]} ${ONES[minute[1] - 1]}`
+            return `${TENS[parseInt(minute[0]) - 1]} ${ONES[parseInt(minute[1]) - 1]}`
     }
 }
+
+const handleTimeSubmit = () => {
+    const hourInput = document.querySelector('#hour-input');
+    const minuteInput = document.querySelector('#minute-input');
+    const time = `${hourInput.value}:${minuteInput.value}`;
+    anglicisedTime.textContent = angliciseTime(time);
+}
+
+submitTime.addEventListener('click', handleTimeSubmit);
