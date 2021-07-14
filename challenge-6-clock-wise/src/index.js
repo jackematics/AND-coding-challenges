b351740f8f2931d7283f5b7d141dddcbb634a055
Jack Rimmer <jackrimmer2996@hotmail.co.uk>
2021-07-14 17:38:29 +0100

    Made code more testable, ensured tests passed

--- challenge-6-clock-wise/src/index.js
+++ challenge-6-clock-wise/src/index.js
@@ -5,7 +5,7 @@ const ONES = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'n
 const TEENS = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const TENS = ['ten', 'twenty', 'thirty', 'forty', 'fifty'];
 
-const angliciseTime = (time) => {
+export const angliciseTime = (time) => {
     const hour = time.substring(0, 2);
     const minute = time.substring(3, 5);
     const meridiem = parseInt(hour) < 12 ? 'am' : 'pm';
@@ -18,7 +18,7 @@ const angliciseTime = (time) => {
         : `It is ${anglicisedHour} ${anglicisedMinute} ${meridiem}.`
 }
 
-const angliciseHour = (hour) => {
+export const angliciseHour = (hour) => {
     hour = twelvifyHour(hour); 
 
     if (hour[0] === '0')
@@ -29,7 +29,7 @@ const angliciseHour = (hour) => {
         return TENS[0];
 }
 
-const twelvifyHour = (hour) => {
+export const twelvifyHour = (hour) => {
     let twelvifiedHour = parseInt(hour) % 12;
     if (twelvifiedHour === 0)
         twelvifiedHour = 12;
@@ -40,7 +40,7 @@ const twelvifyHour = (hour) => {
     return twelvifiedHour.toString();
 }
 
-const angliciseMinute = (minute) => {
+export const angliciseMinute = (minute) => {
     if (minute[0] === '0')
         return minute[1] === '0' ? `` : `o' ${ONES[parseInt(minute[1]) - 1]}`;
     else if (minute[0] === '1' && minute[1] !== '0')
@@ -54,128 +54,131 @@ const angliciseMinute = (minute) => {
 }
 
 const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
 
-const timeIncrementArrow = [
-    { x: 765, y: 266 },
-    { x: 780, y: 281 },
-    { x: 750, y: 281 }
-]
+if (canvas) {
+    const ctx = canvas.getContext('2d');
 
-const timeDecrementArrow = [
-    { x: 750, y: 286 },
-    { x: 780, y: 286 },
-    { x: 765, y: 301 }
-]
-
-const timeSetButton = {
-    centreX: 593,
-    centreY: 295,
-    radiuxX: 15,
-    radiusY: 10
-}
-
-const time = {
-    hour: '00',
-    minute: '00'
-}
-
-const digitStartingPositions = [
-    { x: 625, y: 270 },
-    { x: 650, y: 270 },
-    { x: 690, y: 270 },
-    { x: 715, y: 270 }
-];
-
-const dots = [
-    { centreX: 680, centreY: 277, radius: 2 },
-    { centreX: 680, centreY: 292, radius: 2 }
-];
-
-const clockDisplay = new ClockDisplay(ctx, time, timeIncrementArrow, timeDecrementArrow, digitStartingPositions, dots);
-
-const drawBackground = (url) => {
-    return new Promise((resolve) => {
-        const background = new Image();
-        background.addEventListener("load", () => { resolve(background); });
-        background.src = url;
-    }) 
-}
-
-const drawBedroom = () => {
-    drawBackground('./src/assets/bedroom.png')
-    .then((background) => {
-        ctx.drawImage(background, 0, 0, 1011, 762);
-        clockDisplay.drawClockDisplay();
-    })
-}
-
-drawBedroom();
-
-const handleCanvasClick = (canvas, e) => {
-    e.preventDefault();
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    clockDisplay.setEnglishTime('');
-
-    if (isInsideTriangle(
-        timeIncrementArrow[0].x, timeIncrementArrow[0].y, 
-        timeIncrementArrow[1].x, timeIncrementArrow[1].y,
-        timeIncrementArrow[2].x, timeIncrementArrow[2].y,
-        x, y)) {
-
-        clockDisplay.incrementTime();
-        drawBedroom();
+    const timeIncrementArrow = [
+        { x: 765, y: 266 },
+        { x: 780, y: 281 },
+        { x: 750, y: 281 }
+    ]
+    
+    const timeDecrementArrow = [
+        { x: 750, y: 286 },
+        { x: 780, y: 286 },
+        { x: 765, y: 301 }
+    ]
+    
+    const timeSetButton = {
+        centreX: 593,
+        centreY: 295,
+        radiuxX: 15,
+        radiusY: 10
     }
-    else if (isInsideTriangle(
-        timeDecrementArrow[0].x, timeDecrementArrow[0].y, 
-        timeDecrementArrow[1].x, timeDecrementArrow[1].y,
-        timeDecrementArrow[2].x, timeDecrementArrow[2].y,
-        x, y)) {
-
-        clockDisplay.decrementTime();
-        drawBedroom();
+    
+    const time = {
+        hour: '00',
+        minute: '00'
     }
-    else if (isInsideEllipse(
-        timeSetButton.centreX, timeSetButton.centreY,
-        timeSetButton.radiuxX, timeSetButton.radiusY,
-        x, y
-    )) {
-        mouseDone();
-        handleTimeSubmit();
+    
+    const digitStartingPositions = [
+        { x: 625, y: 270 },
+        { x: 650, y: 270 },
+        { x: 690, y: 270 },
+        { x: 715, y: 270 }
+    ];
+    
+    const dots = [
+        { centreX: 680, centreY: 277, radius: 2 },
+        { centreX: 680, centreY: 292, radius: 2 }
+    ];
+    
+    const clockDisplay = new ClockDisplay(ctx, time, timeIncrementArrow, timeDecrementArrow, digitStartingPositions, dots);
+    
+    const drawBackground = (url) => {
+        return new Promise((resolve) => {
+            const background = new Image();
+            background.addEventListener("load", () => { resolve(background); });
+            background.src = url;
+        }) 
     }
-}
-
-const handleTimeSubmit = () => {
-    const hourInput = clockDisplay.getTime().hour;
-    const minuteInput = clockDisplay.getTime().minute;
-
-    const time = `${hourInput}:${minuteInput}`;
-    const englishTime = angliciseTime(time);
-
-    clockDisplay.setEnglishTime(englishTime);
+    
+    const drawBedroom = () => {
+        drawBackground('./src/assets/bedroom.png')
+        .then((background) => {
+            ctx.drawImage(background, 0, 0, 1011, 762);
+            clockDisplay.drawClockDisplay();
+        })
+    }
+    
     drawBedroom();
-
-    const synth = window.speechSynthesis;
-    const utter = new SpeechSynthesisUtterance(englishTime.toLocaleUpperCase());
-    utter.lang = 'en-gb'
-
-    synth.speak(utter);
+    
+    const handleCanvasClick = (canvas, e) => {
+        e.preventDefault();
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        clockDisplay.setEnglishTime('');
+    
+        if (isInsideTriangle(
+            timeIncrementArrow[0].x, timeIncrementArrow[0].y, 
+            timeIncrementArrow[1].x, timeIncrementArrow[1].y,
+            timeIncrementArrow[2].x, timeIncrementArrow[2].y,
+            x, y)) {
+    
+            clockDisplay.incrementTime();
+            drawBedroom();
+        }
+        else if (isInsideTriangle(
+            timeDecrementArrow[0].x, timeDecrementArrow[0].y, 
+            timeDecrementArrow[1].x, timeDecrementArrow[1].y,
+            timeDecrementArrow[2].x, timeDecrementArrow[2].y,
+            x, y)) {
+    
+            clockDisplay.decrementTime();
+            drawBedroom();
+        }
+        else if (isInsideEllipse(
+            timeSetButton.centreX, timeSetButton.centreY,
+            timeSetButton.radiuxX, timeSetButton.radiusY,
+            x, y
+        )) {
+            mouseDone();
+            handleTimeSubmit();
+        }
+    }
+    
+    const handleTimeSubmit = () => {
+        const hourInput = clockDisplay.getTime().hour;
+        const minuteInput = clockDisplay.getTime().minute;
+    
+        const time = `${hourInput}:${minuteInput}`;
+        const englishTime = angliciseTime(time);
+    
+        clockDisplay.setEnglishTime(englishTime);
+        drawBedroom();
+    
+        const synth = window.speechSynthesis;
+        const utter = new SpeechSynthesisUtterance(englishTime.toLocaleUpperCase());
+        utter.lang = 'en-gb'
+    
+        synth.speak(utter);
+    }
+    
+    let timer = null;
+    
+    const mouseDone = () => {
+        clearInterval(timer);
+    }
+    
+    canvas.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        timer = setInterval(() => {
+            handleCanvasClick(canvas, e)
+        }, 50)
+    });
+    
+    canvas.addEventListener("mouseup", mouseDone);
+    canvas.addEventListener("mouseleave", mouseDone);
 }
-
-let timer = null;
-
-const mouseDone = () => {
-    clearInterval(timer);
-}
-
-canvas.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    timer = setInterval(() => {
-        handleCanvasClick(canvas, e)
-    }, 50)
-});
-
-canvas.addEventListener("mouseup", mouseDone);
-canvas.addEventListener("mouseleave", mouseDone);
