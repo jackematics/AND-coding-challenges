640f3cf7438247c5f1b46e0d7a8dccec2ed4c737
Jack <jackrimmer2996@hotmail.co.uk>
2022-03-20 17:15:08 +0000

    Game rules complete

--- 2022/challenge-04-connect-four/src/game-engine/connect-four.spec.ts
+++ 2022/challenge-04-connect-four/src/game-engine/connect-four.spec.ts
@@ -13,6 +13,11 @@ describe("ConnectFour", () => {
     }
   });
 
+  it("should be 4 consecutive disks for the win condition", () => {
+    const connectFour = new ConnectFour();
+    expect(connectFour.getWinCount()).toBe(4);
+  });
+
   describe("When dropping disks", () => {
     it("should error if the column is less than the grid bounds", () => {
       const connectFour = new ConnectFour();
@@ -49,6 +54,236 @@ describe("ConnectFour", () => {
 
       expect(connectFour.getGrid()[5][3]).toBe(Disk.RED);
       expect(connectFour.getGrid()[4][3]).toBe(Disk.YELLOW);
+    });
+  });
+
+  describe("When calculating the next available row index", () => {
+    it("should be 5 for an empty column", () => {
+      const connectFour = new ConnectFour();
+
+      expect(connectFour.calculateRowDropIndex(0)).toBe(5);
+    });
+
+    it("should be 4 for a column with one disk in it", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.calculateRowDropIndex(0)).toBe(4);
+    });
+
+    it("should be 0 for a column with one space left", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.calculateRowDropIndex(0)).toBe(0);
+    });
+
+    it("should be -1 for a column with no spaces left", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.calculateRowDropIndex(0)).toBe(-1);
+    });
+  });
+
+  describe("When calculating a connect-four", () => {
+    it("should return true if there are four consecutive horizontal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return false if there are not four consecutive horizontal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(false);
+    });
+
+    it("should return true if there are four consecutive vertical disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return false if there are not four consecutive vertical disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(false);
+    });
+
+    it("should return true if there are four consecutive up right diagonal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return true if there are four consecutive up right diagonal disks higher in the grid", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.RED);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return false if there are not four consecutive up right diagonal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(0, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(false);
+    });
+
+    it("should return true if there are four consecutive down left diagonal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(3, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.RED);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return true if there are four consecutive up left diagonal disks higher in the grid", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.YELLOW);
+      connectFour.dropDiskInColumn(3, Disk.RED);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.YELLOW);
+      connectFour.dropDiskInColumn(4, Disk.RED);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.YELLOW);
+      connectFour.dropDiskInColumn(5, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(true);
+    });
+
+    it("should return false if there are not four consecutive up left diagonal disks", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(3, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      expect(connectFour.isConnectFour(Disk.RED)).toBe(false);
+    });
+  });
+
+  describe("reset()", () => {
+    it("should reinitalise the grid", () => {
+      const connectFour = new ConnectFour();
+
+      connectFour.dropDiskInColumn(3, Disk.RED);
+      connectFour.dropDiskInColumn(2, Disk.YELLOW);
+      connectFour.dropDiskInColumn(2, Disk.RED);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(1, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.YELLOW);
+      connectFour.dropDiskInColumn(0, Disk.RED);
+
+      connectFour.reset();
+
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 7; j++) {
+          expect(connectFour.getGrid()[i][j]).toBe(Disk.EMPTY);
+        }
+      }
     });
   });
 });
--- 2022/challenge-04-connect-four/src/game-engine/connect-four.ts
+++ 2022/challenge-04-connect-four/src/game-engine/connect-four.ts
@@ -2,17 +2,20 @@ import Disk from "./disk";
 import GridIndex from "./grid-index";
 
 class ConnectFour {
+  private readonly winCount = 4;
   private readonly gridRowCount = 6;
   private readonly gridColumnCount = 7;
-
   private grid: Disk[][] = [];
-
   private lastPlacedDiskIndex: GridIndex = {
     row: -1,
     column: -1,
   };
 
   constructor() {
+    this.initialiseGrid();
+  }
+
+  public initialiseGrid(): void {
     for (let row: number = 0; row < this.gridRowCount; row++) {
       const row: Disk[] = [];
       for (let column: number = 0; column < this.gridColumnCount; column++) {
@@ -27,10 +30,19 @@ class ConnectFour {
     return this.grid;
   }
 
+  public getWinCount(): number {
+    return this.winCount;
+  }
+
   public dropDiskInColumn(column: number, disk: Disk): void {
     this.handleDropErrors(column, disk);
 
     const rowDropIndex: number = this.calculateRowDropIndex(column);
+    this.lastPlacedDiskIndex = {
+      row: rowDropIndex,
+      column,
+    };
+
     this.grid[rowDropIndex][column] = disk;
   }
 
@@ -44,19 +56,139 @@ class ConnectFour {
     }
   }
 
-  private calculateRowDropIndex(column: number): number {
-    for (let i: number = this.gridRowCount - 1; i >= 0; i--) {
-      if (this.grid[i][column] === Disk.EMPTY) {
-        this.lastPlacedDiskIndex = {
-          row: i,
-          column,
-        };
-
-        return i;
+  public calculateRowDropIndex(column: number): number {
+    for (let row: number = this.gridRowCount - 1; row >= 0; row--) {
+      if (this.grid[row][column] === Disk.EMPTY) {
+        return row;
       }
     }
 
-    throw new Error("Cannot drop disk into a full column");
+    return -1;
+  }
+
+  public isConnectFour(disk: Disk): boolean {
+    return (
+      this.isHorizontalConnectFour(disk) ||
+      this.isVerticalConnectFour(disk) ||
+      this.isUpRightDiagonalConnectFour(disk) ||
+      this.isUpLeftDiagonalConnectFour(disk)
+    );
+  }
+
+  private isHorizontalConnectFour(disk: Disk): boolean {
+    let consecutive: number = 0;
+    for (let column: number = 0; column < this.gridColumnCount; column++) {
+      consecutive =
+        this.grid[this.lastPlacedDiskIndex.row][column] === disk
+          ? consecutive + 1
+          : 0;
+
+      if (consecutive === this.winCount) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private isVerticalConnectFour(disk: Disk): boolean {
+    let consecutive: number = 0;
+    for (let row: number = 0; row < this.gridRowCount; row++) {
+      consecutive =
+        this.grid[row][this.lastPlacedDiskIndex.column] === disk
+          ? consecutive + 1
+          : 0;
+
+      if (consecutive === this.winCount) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private isUpRightDiagonalConnectFour(disk: Disk): boolean {
+    let consecutive: number = 0;
+    const currentGridIndex: GridIndex =
+      this.calculateInitialDownLeftGridIndex();
+
+    while (
+      currentGridIndex.row !== -1 &&
+      currentGridIndex.column !== this.gridColumnCount
+    ) {
+      consecutive =
+        this.grid[currentGridIndex.row][currentGridIndex.column] === disk
+          ? consecutive + 1
+          : 0;
+
+      if (consecutive === this.winCount) {
+        return true;
+      }
+
+      currentGridIndex.row--;
+      currentGridIndex.column++;
+    }
+
+    return false;
+  }
+
+  private isUpLeftDiagonalConnectFour(disk: Disk): boolean {
+    let consecutive: number = 0;
+    const currentGridIndex: GridIndex =
+      this.calculateInitialDownRightGridIndex();
+
+    while (currentGridIndex.row !== -1 && currentGridIndex.column !== -1) {
+      consecutive =
+        this.grid[currentGridIndex.row][currentGridIndex.column] === disk
+          ? consecutive + 1
+          : 0;
+
+      if (consecutive === this.winCount) {
+        return true;
+      }
+
+      currentGridIndex.row--;
+      currentGridIndex.column--;
+    }
+
+    return false;
+  }
+
+  private calculateInitialDownLeftGridIndex(): GridIndex {
+    let currentGridIndex: GridIndex = JSON.parse(
+      JSON.stringify(this.lastPlacedDiskIndex)
+    );
+
+    while (
+      currentGridIndex.row !== this.gridRowCount - 1 &&
+      currentGridIndex.column !== 0
+    ) {
+      currentGridIndex.row++;
+      currentGridIndex.column--;
+    }
+
+    return currentGridIndex;
+  }
+
+  private calculateInitialDownRightGridIndex(): GridIndex {
+    let currentGridIndex: GridIndex = JSON.parse(
+      JSON.stringify(this.lastPlacedDiskIndex)
+    );
+
+    while (
+      currentGridIndex.row !== this.gridRowCount - 1 &&
+      currentGridIndex.column !== this.gridColumnCount - 1
+    ) {
+      currentGridIndex.row++;
+      currentGridIndex.column++;
+    }
+
+    return currentGridIndex;
+  }
+
+  public reset(): void {
+    this.grid = [];
+    this.initialiseGrid();
   }
 }
 
