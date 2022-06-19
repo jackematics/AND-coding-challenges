363f9a9b437dfd6c460f7a4275c10b4f78d0f99e
Jack <jackrimmer2996@hotmail.co.uk>
2022-06-19 20:06:32 +0100

    Fixed prints

--- 2022/challenge-08-pin/src/main.ts
+++ 2022/challenge-08-pin/src/main.ts
@@ -6,10 +6,16 @@ const challengePin = "97516";
 const unsortedFormattedPossibilities = PossibilityFormatter.formatPossibilities(
   PinCalculator.calculatePossibilities(challengePin)
 );
-console.log("Unsorted solution: ", unsortedFormattedPossibilities);
+console.log(
+  "Unsorted solution (ascending numerical order): ",
+  unsortedFormattedPossibilities
+);
 
 const sortedFormattedPossibilities = PossibilityFormatter.formatPossibilities(
   PinCalculator.calculatePossibilitiesByLikelihood(challengePin)
 );
-console.log("Sorted solution: ", sortedFormattedPossibilities);
+console.log(
+  "Sorted solution (Descending by highest weighting)): ",
+  sortedFormattedPossibilities
+);
 console.log(`The most likely pin is ${sortedFormattedPossibilities[0]}`);
