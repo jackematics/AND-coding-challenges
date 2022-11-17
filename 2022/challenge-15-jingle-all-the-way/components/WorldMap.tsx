33fd259ba1e68a32f257ede0a4fb8cfdb3a00c6e
Jack <jackrimmer2996@hotmail.co.uk>
2022-11-17 19:07:42 +0000

    jingle all the way init with world map

---
+++ 2022/challenge-15-jingle-all-the-way/components/WorldMap.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image';
+
+const WorldMap = () => {
+  return (
+    <>
+      <Image
+        src={'/printable-world-map-hd.jpg'}
+        alt={''}
+        width="1288"
+        height="870"
+      />
+    </>
+  );
+};
+
+export default WorldMap;
