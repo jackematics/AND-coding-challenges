88c3b055a6639db360aa2be8deacd57f8cc5cc45
Jack <jackrimmer2996@hotmail.co.uk>
2022-11-17 22:30:43 +0000

    Heading added and looking pretty

--- 2022/challenge-15-jingle-all-the-way/components/WorldMap.tsx
+++ 2022/challenge-15-jingle-all-the-way/components/WorldMap.tsx
@@ -4,12 +4,19 @@ import Image from 'next/image';
 const WorldMap = () => {
   return (
     <>
-      <Image
-        src={'/printable-world-map-hd.jpg'}
-        alt={''}
-        width="1288"
-        height="870"
-      />
+      <div className="flex justify-center">
+        <h1 className="font-mono text-5xl font-bold leading-relaxed">
+          Jingle All the Way
+        </h1>
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={'/printable-world-map-hd.jpg'}
+          alt={'world-map'}
+          width="1288"
+          height="870"
+        />
+      </div>
     </>
   );
 };
